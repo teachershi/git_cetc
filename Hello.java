0a29b3def892d9bc1d9737b11df4c855715b808d
cetc28 <code_shi@126.com>
2022-05-20 14:40:12 +0800

    master branch modify

--- Hello.java
+++ Hello.java
@@ -1,7 +1,8 @@
 public class Hello{
 	public static void main(String[] args){
 		System.out.println("hello git init ");
-		System.out.println("modify by cetc28");
+		System.out.println("modify by cetc28 !!!");
 		System.out.println("modify by dev branch");
+		System.out.println("modify by master branch");
 	}
 }
